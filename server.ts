6393692050afec81857df7d93fbbde0f1bfb5669
Jaskeen Singh <jaskeensingh@Jaskeens-Mac.local>
2025-02-23 20:06:32 +0530

    Simplfied server creation

---
+++ server.ts
@@ -0,0 +1,20 @@
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const port = process.env.PORT || 3001;
+
+app.prepare().then(() => {
+  const server = createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true);
+    handle(req, res, parsedUrl);
+  });
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+}); 
